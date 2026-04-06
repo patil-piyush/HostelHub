d1661a1831af1ac51df51b2cd3611a76d13d9d69
Piyush <piyushpatil0031@gmail.com>
2026-04-07 00:24:33 +0530

    deployment updates

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,10 +5,17 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: false
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT, // IMPORTANT
+      dialect: "mysql",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     }
-);
+  );
 
 module.exports = sequelize;
