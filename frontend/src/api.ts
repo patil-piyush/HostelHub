e06bfefdefd66ffe8d87a3106244bbe9b363959a
Piyush <piyushpatil0031@gmail.com>
2026-04-07 01:39:02 +0530

    deployment update

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://hostelhub-m1cl.onrender.com/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
